d9bdb677f2c0989ec9425e0814fdb023a78a2d4c
Dominic Moser <llamapagama@gmail.com>
2024-09-29 23:00:25 -0600

    more stuff

--- coming-soon/src/routes/+page.svelte
+++ coming-soon/src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import { LazyBrush } from 'lazy-brush';
 
 	const lazy = new LazyBrush({
-		radius: 5,
+		radius: -7,
 		enabled: true,
 		initialPoint: { x: 0, y: 0 }
 	});
@@ -14,17 +14,40 @@
 
 <div
 	style="left: {xPos}px; top: {yPos}px"
-	class="w-10 h-10 rounded-full bg-white mix-blend-difference absolute cursor-none"
+	class="w-10 h-10 rounded-full bg-white mix-blend-difference absolute cursor-none z-50"
 	role="presentation"
+	on:mousemove={(e) => {
+		lazy.update({ x: e.clientX - 15, y: e.clientY - 15 }, { friction: 0.3 });
+		({ x: xPos, y: yPos } = lazy.getBrushCoordinates());
+	}}
 ></div>
 
 <div
-	class="w-screen h-screen bg-black cursor-none justify-center items-center flex"
+	class="w-screen h-screen bg-black justify-center items-center flex cursor-none"
 	on:mousemove={(e) => {
-		lazy.update({ x: e.clientX, y: e.clientY }, { friction: 0.3 });
+		lazy.update({ x: e.clientX - 15, y: e.clientY - 15 }, { friction: 0.3 });
 		({ x: xPos, y: yPos } = lazy.getBrushCoordinates());
 	}}
 	role="presentation"
 >
-	<div class="w-52 h-52 bg-white">lalal</div>
+	<div class="absolute top-0 left-0 bg-transparent text-[7rem] text-white flex flex-col font-sans">
+		<div
+			class="w-screen text-center text-transparent text-[10rem] bg-clip-text bg-cover bg-mesh5 z-40 tracking-widest mt-32"
+		>
+			TALLY
+		</div>
+		<div
+			class="w-screen text-center text-transparent text-[1rem] bg-clip-text bg-cover bg-mesh5 z-40 tracking-widest"
+		>
+			(coming soon)
+		</div>
+	</div>
+
+	<div
+		class="w-screen h-[35vh] flex flex-row items-center justify-center space-x-5 py-10 mt-[30vh]"
+	>
+		<div class="w-1/4 h-full bg-white rounded-[3rem] bg-mesh4 bg-cover transition"></div>
+		<div class="w-1/4 h-full bg-white rounded-[3rem] bg-mesh2 bg-cover transition"></div>
+		<div class="w-1/4 h-full bg-white rounded-[3rem] bg-mesh3 bg-cover transition"></div>
+	</div>
 </div>
--- coming-soon/tailwind.config.js
+++ coming-soon/tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			backgroundImage: {
+				mesh1: "url('/media/mesh1.png')",
+				mesh2: "url('/media/mesh2.png')",
+				mesh3: "url('/media/mesh3.png')",
+				mesh4: "url('/media/mesh4.png')",
+				mesh5: "url('/media/mesh5.png')",
+				mesh6: "url('/media/mesh6.png')",
+				mesh7: "url('/media/mesh7.png')",
+				mesh8: "url('/media/mesh8.png')",
+				mesh9: "url('/media/mesh9.png')",
+				mesh10: "url('/media/mesh10.png')",
+				mesh11: "url('/media/mesh11.png')"
+			}
+		}
 	},
 	plugins: []
 };
